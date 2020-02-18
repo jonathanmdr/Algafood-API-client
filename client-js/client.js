84c3648fe1568997e63146e5d870db714baef90c
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-18 09:55:53 -0300

    Added authentication client

--- client-js/client.js
+++ client-js/client.js
@@ -1,10 +1,29 @@
+const config = {
+  clientId: "food-analytics",
+  clientSecret: "@food-4n4lytic$",
+  authorizeUrl: "http://localhost:8095/oauth/authorize",
+  tokenUrl: "http://localhost:8095/oauth/token",
+  callbackUrl: "http://localhost:8082",
+  paymentFormsUrl: "http://localhost:8080/v1/payment-forms"
+}
+
+let accessToken = "";
+
 function consultar() {
   $.ajax({
-    url: "http://localhost:8080/payment-forms",
+    url: config.paymentFormsUrl,
     type: "get",
 
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", "Bearer " + accessToken);
+    },
+
     success: function(response) {
-      preencherTabela(response);
+      preencherTabela(response._embedded.paymentForms);
+    },
+
+    error: function(error) {
+      alert("Ocorreu um erro ao tentar realizar a consulta!");
     }
   });
 }
@@ -17,10 +36,14 @@ function cadastrar() {
   console.log(formaPagamentoJson);
 
   $.ajax({
-    url: "http://localhost:8080/payment-forms",
+    url: config.paymentFormsUrl,
     type: "post",
     data: formaPagamentoJson,
     contentType: "application/json",
+
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", "Bearer " + accessToken);
+    },
 
     success: function(response) {
       alert("Forma de pagamento adicionada!");
@@ -39,11 +62,15 @@ function cadastrar() {
 }
 
 function excluir(formaPagamento) {
-  var url = "http://localhost:8080/payment-forms/" + formaPagamento.id;
+  var url = config.paymentFormsUrl + "/" + formaPagamento.id;
 
   $.ajax({
     url: url,
     type: "delete",
+
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", "Bearer " + accessToken);
+    },
 
     success: function(response) {
       consultar();
@@ -65,7 +92,7 @@ function excluir(formaPagamento) {
 
 function preencherTabela(formasPagamento) {
   $("#tabela tbody tr").remove();
-
+  
   $.each(formasPagamento, function(i, formaPagamento) {
     var linha = $("<tr>");
 
@@ -86,5 +113,58 @@ function preencherTabela(formasPagamento) {
   });
 }
 
+function gerarAccessToken(code) {  
+  let clientAuth = btoa(config.clientId + ":" + config.clientSecret);
+  
+  let params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", config.callbackUrl);
+
+  $.ajax({
+    url: config.tokenUrl,
+    type: "post",
+    data: params.toString(),
+    contentType: "application/x-www-form-urlencoded",
+
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", "Basic " + clientAuth);
+    },
+
+    success: function(response) {
+      accessToken = response.access_token;
+    },
+
+    error: function(error) {
+      alert("Erro ao gerar access key");
+    }
+  });
+}
+
+function login() {
+  // https://auth0.com/docs/protocols/oauth2/oauth-state
+  let state = btoa(Math.random());
+  localStorage.setItem("clientState", state);
+
+  window.location.href = `${config.authorizeUrl}?response_type=code&client_id=${config.clientId}&state=${state}&redirect_uri=${config.callbackUrl}`;
+}
+
+$(document).ready(function() {
+  let params = new URLSearchParams(window.location.search);
+
+  let code = params.get("code");
+  let state = params.get("state");
+  let currentState = localStorage.getItem("clientState");
+
+  if (code) {
+    if (currentState == state) {
+      gerarAccessToken(code);
+    } else {
+      alert("State inválido");
+    }
+  }
+});
+
 $("#btn-consultar").click(consultar);
 $("#btn-cadastrar").click(cadastrar);
+$("#btn-login").click(login);
